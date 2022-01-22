b0d3e5c862dc1915f41340ca058e1a82e6420d33
Silvio <jalcoser1996@gmail.com>
2022-01-22 15:02:47 -0500

    ProductsContext

--- App.tsx
+++ App.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigator/Navigator';
 import { AuthProvider } from './src/context/AuthContext';
+import { ProductProvider } from './src/context/ProductsContext';
 
 const AppState = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   return (
+    // AuthProvider es quien gobierna todo la aplicacion
     <AuthProvider>
+      <ProductProvider>
       {children}
+      </ProductProvider>
     </AuthProvider>
   )
 }
