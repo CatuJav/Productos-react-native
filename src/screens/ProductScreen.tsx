c9efd4fb00a29dbe24bbee02c6e438ac13495739
Silvio <jalcoser1996@gmail.com>
2022-02-03 20:53:33 -0500

    Picker

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,77 +1,89 @@
-import { StackScreenProps } from '@react-navigation/stack';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import { ProductsStackParams } from '../navigator/ProductsNavigator';
+import { StackScreenProps } from '@react-navigation/stack';
 
-interface Props extends StackScreenProps<ProductsStackParams,'ProductScreen'>{};
+interface Props extends StackScreenProps<ProductsStackParams, 'ProductScreen'> { };
 
-export const ProductScreen = ({navigation,route}:Props) => {
+export const ProductScreen = ({ navigation, route }: Props) => {
 
-  const {id,name=''}=route.params;
+  const { id, name = '' } = route.params;
+  const [selectedLanguage, setSelectedLanguage] = useState();
 
   useEffect(() => {
-      navigation.setOptions({
-        title:name
-      })
+    navigation.setOptions({
+      title: name
+    })
   }, []);
-  
+
 
   return <View style={styles.container}>
-      <ScrollView>
-        <Text style={styles.label}>Nombre del producto</Text>
-        <TextInput
-          placeholder='Producto'
-          style={styles.textInput}
-          //TODO: value, onChangeText
-        />
-        {/*Picker / Selector*/}
+    <ScrollView>
+      <Text style={styles.label}>Nombre del producto</Text>
+      <TextInput
+        placeholder='Producto'
+        style={styles.textInput}
+      //TODO: value, onChangeText
+      />
+      {/*Picker / Selector*/}
 
-        <Text style={styles.label}>Categoía: </Text>
-        
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+
+
+      <Text style={styles.label}>Categoía: </Text>
+
+      <Button
+
+        title='Guardar'
+        onPress={() => { }}
+        color="#5856d6"
+      />
+
+      <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
         <Button
-        
-          title='Guardar'
-          onPress={()=>{}}
+
+          title='Cámara'
+          onPress={() => { }}
           color="#5856d6"
         />
-        
-        <View style={{flexDirection:'row',justifyContent:'center', marginTop:10}}>
+        <View style={{ width: 10 }}></View>
         <Button
-        
-        title='Cámara'
-        onPress={()=>{}}
-        color="#5856d6"
-      />
-      <View style={{width:10}}></View>
-              <Button
-        
-        title='Galería'
-        onPress={()=>{}}
-        color="#5856d6"
-      />
-        </View>
 
-      </ScrollView>
+          title='Galería'
+          onPress={() => { }}
+          color="#5856d6"
+        />
+      </View>
+
+    </ScrollView>
   </View>;
 };
 
-const styles= StyleSheet.create({
-  container:{
-    flex:1,
-    marginTop:10,
-    marginHorizontal:20
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginHorizontal: 20
   },
-  label:{
-    fontSize:18
+  label: {
+    fontSize: 18
   },
-  textInput:{
-    borderWidth:1,
-    paddingHorizontal:10,
-    paddingVertical:5,
-    borderRadius:20,
-    borderColor:'rgba(0,0,0,0.2)',
-    height:45,
-    marginTop:5,
-    marginBottom:15
+  textInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    borderColor: 'rgba(0,0,0,0.2)',
+    height: 45,
+    marginTop: 5,
+    marginBottom: 15
   }
 })
